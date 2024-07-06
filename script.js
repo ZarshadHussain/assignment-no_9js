9f3b6d68014ed06c756fdec71d4dc5d09f34049d
ZarshadHussain <zarshadhussain719@gmail.com>
2024-07-06 01:11:47 -0700

    js 9 assignment

---
+++ script.js
@@ -0,0 +1,28 @@
+// 1. Print 1 To 10 Table ....
+// var num = +prompt("Enter a Number");
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`${num} * ${i} = ${num * i}`);
+// }
+
+
+// 2. To Print sum Of Array ....
+// var numbers = [1, 2, 3, 4, 5];
+//     for (let i = 0; i < arr.length; i++) {
+//         sum += arr[i];
+//     }
+
+
+// 3. ARRAY KA SUB SE BARA NUMBER PRINT KARE
+// DESCRIPTION:
+// 1. user se 5 numbers le and unko aik aik kar k array me rakhe
+// 2. phr array elements me SUB SE BARA NUMBER find kar k print kar
+    
+// var numbers = [];
+
+// for (let i = 0; i < 5; i++) {
+//     let num = +prompt(prompt(`Enter number ${i + 1}:`));
+// }
+//     let sum = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         sum += arr[i];
+//     }
